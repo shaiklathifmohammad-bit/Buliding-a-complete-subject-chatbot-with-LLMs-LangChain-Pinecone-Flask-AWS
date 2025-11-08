fb10f5432a18d7f75a8a3873a8e6c3a2162dda71
Shaik Lathif Mohammad <shaiklathifmohammad@hmail.com>
2025-11-07 22:07:29 -0500

    requriments added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="subject_bot",
+    version="0.1.0",
+    author="Lathif Shaik",
+    author_email="shaiklathifmohammad@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
